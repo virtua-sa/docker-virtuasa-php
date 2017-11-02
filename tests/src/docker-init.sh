9dd4d98dda738b7feca08bbad92da9aca695183b
Jérémy WALTHER <jeremy.walther@virtua.ch>
2017-11-02 18:09:32 +0100

    Print versions on each tests

--- tests/src/docker-init.sh
+++ tests/src/docker-init.sh
@@ -1,2 +1,16 @@
 #!/bin/bash
 echo "docker-init.sh called !"
+
+[[ -n "$(type -t php)" ]]       && php -v
+[[ -n "$(type -t composer)" ]]  && composer --version
+[[ -n "$(type -t phpunit62)" ]] && phpunit62 --version
+[[ -n "$(type -t phpunit57)" ]] && phpunit57 --version
+[[ -n "$(type -t phpunit48)" ]] && phpunit48 --version
+[[ -n "$(type -t phpcs)" ]]     && phpcs --version
+[[ -n "$(type -t phpcbf)" ]]    && phpcbf --version
+[[ -n "$(type -t pdepend)" ]]   && pdepend --version
+[[ -n "$(type -t phpmd)" ]]     && phpmd --version
+[[ -n "$(type -t phpcpd)" ]]    && phpcpd --version
+
+[[ -n "$(type -t node)" ]]      && echo -n "Node.js " && node -v
+[[ -n "$(type -t npm)" ]]       && echo -n "NPM v" && npm -v
