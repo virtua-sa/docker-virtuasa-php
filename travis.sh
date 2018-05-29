9945348ab152cfe4298bd7aeac2ecba56f8a9664
Rui Filipe Da Cunha Alves <rui@virtua.ch>
2018-05-29 10:08:54 +0200

    travis compatibility

--- travis.sh
+++ travis.sh
@@ -2,12 +2,12 @@
 
 PHP_VERSION=$1
 
-di_disable_push=1 ./docker-build.sh ${PHP_VERSION}
-RESULT=$?
+echo "Docker Login"
+docker login -u ${DOCKER_USERNAME} -p ${DOCKER_PASSWORD}
 
-if [ ${RESULT} -eq 0 ] && [ ${TRAVIS_BRANCH} -eq 'master' ] ; then
-    echo "Docker Login"
-    docker login -u ${DOCKER_USERNAME} -p ${DOCKER_PASSWORD}
+if [ ${TRAVIS_BRANCH} -eq 'master' ] ; then
+    # build no push
+    di_disable_push=1 ./docker-build.sh ${PHP_VERSION}
     RESULT=$?
 
     if [ ${RESULT} -eq 0 ]  ; then
@@ -15,6 +15,14 @@ if [ ${RESULT} -eq 0 ] && [ ${TRAVIS_BRANCH} -eq 'master' ] ; then
         ./docker-push.sh ${PHP_VERSION};
         RESULT=$?
     fi
+elif [ ${TRAVIS_BRANCH} -eq 'develop' ] ; then
+    # build and push to dev tag
+    ./docker-build.sh ${PHP_VERSION}
+    RESULT=$?
+else
+    # build no push
+    di_disable_push=1 ./docker-build.sh ${PHP_VERSION}
+    RESULT=$?
 fi
 
 exit ${RESULT};
