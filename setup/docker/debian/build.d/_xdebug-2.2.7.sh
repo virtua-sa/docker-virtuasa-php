d52fc9e67bfeabbe150cd5e08f50c5eee851cd42
Rui Filipe Da Cunha Alves <rui@virtua.ch>
2018-06-06 16:39:39 +0200

    add extention mailparse

--- setup/docker/debian/build.d/_xdebug-2.2.7.sh
+++ setup/docker/debian/build.d/_xdebug-2.2.7.sh
@@ -2,7 +2,7 @@
 set -xe
 
 echo "Compile xdebug with pecl"
-apt-get remove php5-xdebug
+DEBIAN_FRONTEND=noninteractive apt-get remove -y --force-yes php5-xdebug
 pecl install xdebug-2.2.7
 echo '; Enable xdebug extension module' > /etc/php5/conf.d/xdebug.ini
 echo "zend_extension=`find /usr/lib -name xdebug.so | tr -d '[:space:]'`" >> /etc/php5/conf.d/xdebug.ini
