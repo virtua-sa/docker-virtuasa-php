8ad7b16e90fe0d70cb22093484ea749311e58f52
Rui Filipe Da Cunha Alves <rui@virtua.ch>
2018-06-06 17:50:41 +0200

    add extention mailparse

--- setup/docker/debian/build.d/php-5.5.sh
+++ setup/docker/debian/build.d/php-5.5.sh
@@ -3,6 +3,9 @@ set -xe
 
 DIR=$(dirname $0)
 
-${DIR}/_mailparse-2.1.6.sh
+echo "Compile mailparse with pecl"
+pecl install mailparse-2.1.6
+echo '; Enable mailparse extension module' > /etc/php5/mods-available/mailparse.ini
+echo "extension=mailparse.so" >> /etc/php5/mods-available/mailparse.ini
 
 exit 0
