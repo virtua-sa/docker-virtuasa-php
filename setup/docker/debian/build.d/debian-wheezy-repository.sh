53d95d150dee4715ff102e6362725d4e5e607a05
Rui Filipe Da Cunha Alves <rui@virtua.ch>
2019-04-09 08:39:09 +0200

    Remove repository "http://security.debian.org/debian-security wheezy/updates main"

--- setup/docker/debian/build.d/debian-wheezy-repository.sh
+++ setup/docker/debian/build.d/debian-wheezy-repository.sh
@@ -3,5 +3,4 @@ set -xe
 
 cat <<EOT > /etc/apt/sources.list
 deb http://archive.debian.org/debian wheezy main contrib non-free
-deb http://security.debian.org/debian-security wheezy/updates main
 EOT
