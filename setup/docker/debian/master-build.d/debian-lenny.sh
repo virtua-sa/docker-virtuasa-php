42ccbda214915b857bf01e58fa8dedba328c965f
Rui Filipe Da Cunha Alves <rui@virtua.ch>
2019-09-18 16:44:09 +0200

    add debian buster for php 7.3

--- setup/docker/debian/master-build.d/debian-lenny.sh
+++ setup/docker/debian/master-build.d/debian-lenny.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 set -xe
 
-echo "Download openssl-1.1.1d.tar.gz"
-wget -q -P setup/tmp https://www.openssl.org/source/openssl-1.1.1d.tar.gz || exit 1
-echo "Download curl-7.66.0.tar.gz"
-wget -q -P setup/tmp https://curl.haxx.se/download/curl-7.66.0.tar.gz || exit 1
+echo "Download openssl-1.1.0.tar.gz"
+wget -q -P setup/tmp https://www.openssl.org/source/openssl-1.1.0.tar.gz || exit 1
+echo "Download curl-7.52.1.tar.gz"
+wget -q -P setup/tmp https://curl.haxx.se/download/curl-7.52.1.tar.gz || exit 1
 
 exit 0
