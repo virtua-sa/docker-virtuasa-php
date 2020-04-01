061546e411360b1891c1e7e4c5158013e431039c
Rui Filipe Da Cunha Alves <rui@virtua.ch>
2020-04-01 13:28:13 +0200

    Fix php 5.* < 5.6 with custom compilation of curl

--- setup/docker/debian/build.d/_compile_openssl_curl.sh
+++ setup/docker/debian/build.d/_compile_openssl_curl.sh
@@ -3,8 +3,11 @@ set -xe
 
 # install OpenSSL 1.0.1
 DEBIAN_FRONTEND=noninteractive apt-get upgrade -y --force-yes
-DEBIAN_FRONTEND=noninteractive apt-get install -y --force-yes libssl-dev info2man curl
+DEBIAN_FRONTEND=noninteractive apt-get install -y --force-yes libssl-dev info2man pkg-config libtool
 DEBIAN_FRONTEND=noninteractive apt-get -y --force-yes remove curl
+
+# Remove wrong OpenSSL headers
+rm -Rf /usr/lib/x86_64-linux-gnu/openssl-*
 
 echo "Build OpenSSL..."
 tar xfz /setup/tmp/openssl-*
@@ -20,7 +23,7 @@ openssl version
 echo "Build Curl..."
 tar xfz /setup/tmp/curl-*
 cd curl-*
-./configure --disable-shared > log-file 2>&1 || (cat log-file; exit 1)
+CPPFLAGS="-I/usr/include/openssl" LDFLAGS="-L/usr/lib/" ./configure --disable-shared > log-file 2>&1 || (cat log-file; exit 1)
 make > log-file 2>&1 || (cat log-file; exit 1)
 make install > log-file 2>&1 || (cat log-file; exit 1)
 appdir=$(pwd)
@@ -29,6 +32,6 @@ rm -Rf "${appdir}"
 ln -s /usr/local/bin/curl /usr/bin/curl
 curl --version
 
-DEBIAN_FRONTEND=noninteractive apt-get purge -y --force-yes libssl-dev info2man
+DEBIAN_FRONTEND=noninteractive apt-get purge -y --force-yes libssl-dev info2man pkg-config libtool
 
 exit 0
--- setup/docker/debian/master-build.d/_download_openssl_curl.sh
+++ setup/docker/debian/master-build.d/_download_openssl_curl.sh
@@ -2,7 +2,7 @@
 set -xe
 
 echo "Download openssl-1.1.0f.tar.gz"
-[[ ! -f setup/tmp/openssl-1.1.0f.tar.gz ]] && (wget -q -P setup/tmp https://www.openssl.org/source/openssl-1.1.0f.tar.gz || exit 1)
+[[ ! -f setup/tmp/openssl-1.1.0f.tar.gz ]] && (wget -q -P setup/tmp https://ftp.openssl.org/source/old/1.1.1/openssl-1.1.1c.tar.gz || exit 1)
 echo "Download curl-7.52.1.tar.gz"
 [[ ! -f setup/tmp/curl-7.52.1.tar.gz ]] && (wget -q -P setup/tmp https://curl.haxx.se/download/curl-7.52.1.tar.gz || exit 1)
 
